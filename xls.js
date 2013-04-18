eb0fa1ca5fce38513aedb0a9436cdcf81f895c6b
Niggler <me@nig.gl>
2013-04-18 11:36:01 -0400

    added buffer.concat implementation
    (not found in node 0.6)

--- xls.js
+++ xls.js
@@ -1,5 +1,35 @@
 /* vim: set ts=2: */
 /*jshint eqnull:true */
+/* Buffer.concat was added in the 0.8 series, so this is for older versions */
+if(typeof Buffer !== "undefined" && !Buffer.concat) 
+Buffer.concat = function(list, length) {
+	if (!Array.isArray(list)) {
+		throw new TypeError('Usage: Buffer.concat(list, [length])');
+	}
+
+	if (list.length === 0) {
+		return new Buffer(0);
+	} else if (list.length === 1) {
+		return list[0];
+	}
+
+	if (typeof length !== 'number') {
+		length = 0;
+		for (var i = 0; i < list.length; i++) {
+			var buf = list[i];
+			length += buf.length;
+		}
+	}
+
+	var buffer = new Buffer(length);
+	var pos = 0;
+	for (var i = 0; i < list.length; i++) {
+		var buf = list[i];
+		buf.copy(buffer, pos);
+		pos += buf.length;
+	}
+	return buffer;
+};
 
 var Buffers = Array;
 
