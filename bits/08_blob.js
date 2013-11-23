6e2a1493084977249a77d0a469c88d6091530cc4
SheetJS <dev@sheetjs.com>
2013-11-23 13:59:45 -0800

    s2a enforces single byte restriction
    h/t @rickosborne.  Fixes #21
    
    See https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Sending_and_Receiving_Binary_Data#Receiving_binary_data_in_older_browsers for more info

--- bits/08_blob.js
+++ bits/08_blob.js
@@ -63,7 +63,7 @@ var Base64 = (function(){
 
 function s2a(s) {
 	if(typeof Buffer !== 'undefined') return new Buffer(s, "binary");
-	var w = s.split("").map(function(x){return x.charCodeAt(0);});
+	var w = s.split("").map(function(x){ return x.charCodeAt(0) & 0xff; });
 	return w;
 }
 
