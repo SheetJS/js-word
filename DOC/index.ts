99dc9215dc31b07b5d8503bcf5116b7c0eef1cd0
Srijon Saha <srijon365@gmail.com>
2020-07-09 16:46:13 -0700

    Add DOC parser

---
+++ DOC/index.ts
@@ -0,0 +1,20 @@
+import { read, find } from "cfb";
+import { readFib } from "./fib";
+import { getDocTxt } from "./clx";
+
+/**
+ * [MS-DOC] 2.4.1 Retrieving Text
+ */
+function parseDoc(filePath: string): string {
+  const file = read(filePath, {type: "file"});
+  const wordDocument = find(file, "/WordDocument");
+  const wordStream = wordDocument.content as Buffer;
+  const fib = readFib(wordStream);
+  const tableName = fib.base.fWhichTblStm === 1 ? "/1Table" : "/0Table";
+  const table = find(file, tableName);
+  const tableStream = table.content as Buffer;
+  const text = getDocTxt(fib, wordStream, tableStream);
+  return text;
+}
+
+console.log(parseDoc("test1.doc"));
