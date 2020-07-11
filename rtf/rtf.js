6a1f86a388369fef6ee4945ff3514d1d49253e2d
William Law <williamlaw.wtl@gmail.com>
2020-07-11 01:12:02 -0400

    add: rtf parsing script (#123)

---
+++ rtf/rtf.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+
+// var pairs = [];
+// var seen_par = false;
+var blacklist = ["\\fonttbl", "\\rtlch", "\\fcs1", "\\af0", "\\ltrch", "\\fcs0", "\\insrsid"];
+var counter = [];
+var offsets = [];
+var para_text = [];
+var current_par = "";
+var par_start = -1;
+var last_brace = Infinity
+var skiplength = Infinity; // keep track of skip length
+
+/* 
+    TEST SAMPLES
+*/
+// var rtf = "{\\rtf1\\ansi\\deff0{\\fonttbl{\\f0\\froman Tms Rmn;}}"
+// var rtf = "{\\par The word \'93}{\\cs15\\b\\ul\\cf6 style}{\'94 is red and underlined. I used a style I called UNDERLINE.\\par }"
+// var rtf = "{\\rtlch\\fcs1 \\af31507 \\ltrch\\fcs0 \\insrsid6445377 Hello}{\\rtlch\\fcs1 \\af31507 \\ltrch\\fcs0 \\insrsid5199918 \\par }{\\rtlch\\fcs1 \\af31507 \\ltrch\\fcs0 \\insrsid6445377 There\\par }"
+// var rtf = "{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid6445377 Hello}{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid3943939 \\par }{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid5116832 aaa}{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid6445377 \\par }"
+var rtf = "{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid6445377 Hello}{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid11279206  ba}{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid3943939 \\par }{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid5116832 aaa}"
+// var rtf = "{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid6445377 Hello}{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid11279206  ba}{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid14709222  bok}{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid3943939 \\par }{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \\insrsid5116832 aaa}{\\rtlch\\fcs1 \\af0 \\ltrch\\fcs0 \insrsid6445377 \\par }"
+
+var extract_text = rtf.replace(/({|\\[A-Za-z\d]+|}?\b\w+\b)/g, function ($$, $1, idx) {
+    if ($$ == "{") {
+        counter.push(0);
+        offsets.push(idx);
+        last_brace = idx;
+
+        return "{";
+    } else if ($$ == "}") {
+        counter.pop();
+        offsets.pop();
+        last_brace = -1;
+
+        // if (seen_par) {
+        //   console.log(pairs)
+        // }
+
+        if (counter.length < skiplength) {
+            skiplength = Infinity; // reset if we exited
+            if (par_start > -1) {
+                var par_snippet = rtf.slice(par_start + "\\par".length, idx);
+                par_start = -1;
+                current_par += par_snippet;
+            }
+        }
+
+        return "}";
+    } else if (blacklist.indexOf($$) == -1) {
+        var text = $$.replace(/\\\w+ ?/g, ""); // .match(/\b\w+\b/g)
+        if (text !== null) {
+            para_text.push(text[0]);
+        }
+    } else if (counter.length > skiplength) {
+        return ""; // skip
+    } else {
+        if (counter[counter.length - 1]++ == 0) { // counter (stack) is empty
+            if (blacklist.indexOf($$) > -1) { // if word found in blacklist
+                skiplength = counter.length;
+                return ""; // skip   - found in blacklist arr
+            } else if ($$ == "\\par") { // Control word found
+                // +1 for the length of `{`
+                current_par += rtf.slice(offsets[offsets.length - 1] + 1, idx)
+
+                // if (par_start > -1) {
+                //    console.log(rtf.slice(offsets.peak(), idx)) // The text between \par tags
+                // }
+
+                // if (!seen_par) {
+                //    seen_par = true;
+                // }
+
+                par_start = idx; // mark where \\par begins
+                skiplength = counter.length; // skip
+                pairs.push($$)
+
+                para_text.push(current_par)
+                current_par = ""
+            }
+        } else if ($$ == "\\par") {
+            current_par += rtf.slice(offsets[offsets.length - 1] + 1, idx);
+            para_text.push(current_par)
+            current_par = ""
+        }
+    }
+});
+
+console.log(para_text);
