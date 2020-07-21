d63b1fd885bbb0b3bb433cf67bd76345462b868a
wlawt <williamlaw.wtl@gmail.com>
2020-07-21 13:04:29 -0400

    add: more rtf test files

---
+++ cross_ext.ps1
@@ -0,0 +1,113 @@
+$HelpCommand = "Run '.\generate_txt.ps1 -h' to see examples"
+
+#Check if argument or option is provided
+if ($Args.count -ne 1) {
+    Write-Output "ERROR: Provide a test file directory or options. $HelpCommand"
+    exit
+}
+
+# Check if argument is an option
+if ($Args[0] -match '^-') {
+    #Check if option is help
+    if ($Args[0] -match '^-[h|H](elp)?$') {
+        Write-Output "Usage:
+        .\generate_txt[.ps1] <filePath>
+        .\generate_txt[.ps1] -[h|H[elp]]
+        Examples:
+        filePath = .\test_files\docx\apachepoi
+        "
+    } else {
+        Write-Output "ERROR: Provide a valid option. $HelpCommand"
+        exit
+    }
+}
+
+# Check if path exists
+if (Test-Path $Args[0]) {
+    $Directory = Resolve-Path $Args[0]
+} else {
+    Write-Output "ERROR: Provide a valid test file directory. $HelpCommand"
+    exit
+}
+
+$Directory = Resolve-Path $Args[0]
+$LineEnding = 1 # WdLineEndingType 1 = CROnly
+$Encoding = 65001 # Codepage for UTF8
+$parent = Split-Path (Split-Path $Directory -Parent) -Leaf
+$subparent = Split-Path $Directory -Leaf
+
+<# 
+    Convert current extension to `.rtf`
+#>
+$CurrAbsPath = @(Get-ChildItem -path $Directory -Recurse -Exclude *.txt, *.skip)
+:curr_main for($i=0; $i -lt $CurrAbsPath.length; $i++) {
+    $CurrAbsPathI = Resolve-Path $CurrAbsPath[$i] | Select-Object -ExpandProperty Path
+ 
+    try {
+        $similarAbsPath = Join-Path -Path .\test_files\rtf -ChildPath $parent\$subparent
+        $filename = Split-Path $CurrAbsPathI -Leaf
+        $similarAbsFilePath = Resolve-Path (Join-Path -Path $similarAbsPath -ChildPath ($filename+".rtf"))
+
+        if (Test-Path $similarAbsFilePath -PathType Leaf) { 
+            Write-Output "RTF already exists"
+            continue curr_main
+        }
+    } catch {
+        continue curr_main
+    }
+
+    # if (Test-Path ($CurrAbsPathI + ".rtf") -PathType Leaf ) { continue curr_main }
+ 
+    if (Test-Path ($CurrAbsPathI + ".skip")) { continue curr_main }
+ 
+    $Word = New-Object -ComObject Word.Application
+    try {
+        $Doc = $Word.Documents.Open($CurrAbsPathI, $False, $True, $False, "WordJS", "WordJS")
+        $Doc.SaveAs(($CurrAbsPathI+".rtf"), 6, $False, "", $False, "", $False, $False, $False, $False, $False, $Encoding, $False, $False, $LineEnding)
+        $Doc.Close()
+    } catch {
+        Write-Output "Skipping (has pwd): $CurrAbsPathI"
+    }
+ 
+    Stop-Process -Name "winword"
+}
+
+<# 
+    Save a `.txt` file of the `.rtf` file that 
+    was just created. 
+
+    Then mirror the folder structure into the `rtf/` folder
+    and move the `.rtf` and `.txt` files into the subfolder.
+#> 
+$ExtAbsPath = @(Get-ChildItem -path $Directory -Recurse -Exclude *.txt, *.skip, *.docx)
+$rtfPath = Join-Path -Path .\test_files\ -ChildPath rtf
+:ext_main for($i=0; $i -lt $ExtAbsPath.length; $i++) {
+    $ExtAbsPathI = Resolve-Path $ExtAbsPath[$i] | Select-Object -ExpandProperty Path
+
+    if ((Get-Item $ExtAbsPathI).length > 10000000) {
+        Remove-Item $ExtAbsPathI
+        continue ext_main
+    }
+
+    if (Test-Path ($ExtAbsPathI + ".txt") -PathType Leaf ) { continue ext_main }
+ 
+    if (Test-Path ($ExtAbsPathI + ".skip")) { continue ext_main }
+ 
+    New-Item -Path $rtfPath -Name $parent -ItemType "directory" -Force
+    $p_rtfPath = Join-Path -Path $rtfPath -ChildPath $parent
+    New-Item -Path $p_rtfPath -Name $subparent -ItemType "directory" -Force
+    $sp_rtfPath = Join-Path -Path $p_rtfPath -ChildPath $subparent
+
+    $Word = New-Object -ComObject Word.Application
+    try {
+        $Doc = $Word.Documents.Open($ExtAbsPathI, $False, $True, $False, "WordJS", "WordJS")
+        $Doc.SaveAs(($ExtAbsPathI+".txt"), 7, $False, "", $False, "", $False, $False, $False, $False, $False, $Encoding, $False, $False, $LineEnding)
+        $Doc.Close() 
+        Move-Item -Path ($ExtAbsPathI+".txt") -Destination (Resolve-Path $sp_rtfPath) 
+        Move-Item -Path ($ExtAbsPathI) -Destination (Resolve-Path $sp_rtfPath) 
+    } catch {
+        Write-Output "Skipping (has pwd): $ExtAbsPathI"
+    }
+ 
+    Stop-Process -Name "winword"
+}
