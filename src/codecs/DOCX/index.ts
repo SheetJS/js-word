cb1aefdcb50cb625009a9907b0050303ec8f9e4e
Janie <janiewang26@gmail.com>
2020-07-20 17:40:03 -0700

    DOCX skip all unrecognized tags

--- src/codecs/DOCX/index.ts
+++ src/codecs/DOCX/index.ts
@@ -1,56 +1,104 @@
 import { read as readCFB, find, CFB$Container } from "cfb";
 import { JSDOM } from "jsdom";
-import { WJSDoc, WJSPara } from "../../types";
+import { WJSDoc, WJSPara, WJSTable } from "../../types";
 
 /* ECMA 17.3.1.22 p CT_P */
 function process_para(child: Node, root: WJSPara) {
-  switch(child.nodeType) {
+  switch (child.nodeType) {
     case 1 /* ELEMENT_NODE */:
       const element = (child as Element);
-      switch(element.tagName) {
+      switch (element.tagName) {
         case "w:r":
         case "w:sdt":
         case "w:sdtContent":
+        case "w:customXml":
           element.childNodes.forEach((child) => process_para(child, root));
           break;
         case "w:t":
-          root.elts.push({t: "s", v: child.textContent}); break;
+          root.elts.push({ t: "s", v: child.textContent }); break;
         case "w:hyperlink": // TODO: store actual hyperlink?
           element.childNodes.forEach((child) => process_para(child, root));
           break;
         case "w:br":
           break;
-        case "w:pPr":
-        case "w:rPr":
-        case "w:bookmarkStart":
+
+        case "w:annotationRef":
         case "w:bookmarkEnd":
-        case "w:sectPr":
-        case "w:pict":
-        case "w:lastRenderedPageBreak":
+        case "w:bookmarkStart":
+        case "w:commentRangeStart":
+        case "w:commentRangeEnd":
+        case "w:commentReference":
+        case "w:del":
         case "w:drawing":
+        case "w:endnoteReference":
+        case "w:fldChar":
+        case "w:fldSimple":
+        case "w:footnoteReference":
+        case "w:ins":
+        case "w:instrText":
+        case "w:lastRenderedPageBreak":
+        case "w:moveFrom":
+        case "w:moveFromRangeStart":
+        case "w:moveFromRangeEnd":
+        case "w:moveTo":
+        case "w:moveToRangeStart":
+        case "w:moveToRangeEnd":
+        case "w:noBreakHyphen":
+        case "w:object":
+        case "w:pict":
+        case "w:pPr":
+        case "w:proofErr":
+        case "w:rPr":
+        case "w:ruby":
+        case "w:sdtEndPr":
+        case "w:sdtPr":
+        case "w:sectPr":
+        case "w:smartTag":
+        case "w:softHyphen":
+        case "w:sym": //TODO: Read documentation about this
+        case "w:tab":
+        case "mc:AlternateContent":
+        case "m:oMath":
+        case "m:oMathPara":
+        case "w16se:sym":
           break;
         default: throw `DOCX para unsupported ${element.tagName} element`
       }
-    break;
+      break;
   }
 };
 
-function process_body_elt(child: ChildNode, root: boolean = false): WJSPara|void {
-  const para: WJSPara = {elts : []};
-  switch(child.nodeType) {
+function process_table(tablelt: Element): WJSTable {
+  const table: WJSTable = { t: "t", r: [] };
+  return table;
+}
+
+function process_body_elt(child: ChildNode, root: boolean = false): WJSPara | void {
+  const para: WJSPara = { elts: [] };
+  switch (child.nodeType) {
     case 1: /* ELEMENT_NODE */
-    const element = (child as Element);
-    switch(element.tagName) {
-      case "w:p":
-        element.childNodes.forEach((child) => process_para(child, para));
-        return para;
-      case "w:tbl":
-      case "w:customXML":
-        if(root) break;
-      case "w:sectPr": break;
-      default: throw `DOCX body unsupported ${element.tagName} element`
-    }
-    break;
+      const element = (child as Element);
+      switch (element.tagName) {
+        case "w:p":
+          element.childNodes.forEach((child) => process_para(child, para));
+          return para;
+        case "w:tbl":
+          // para.elts.push(process_table(element));
+          console.log(element);
+        case "w:customXML":
+          if (root) break;
+        case "w:sectPr":
+        case "w:bookmarkStart":
+        case "w:bookmarkEnd":
+        case "w:commentRangeEnd":
+        case "w:moveFromRangeEnd":
+        case "w:sdt":
+        case "w:altChunk": //TODO: implicit/explicit link handeling
+        case "mc:AlternateContent":
+          break;
+        default: throw `DOCX body unsupported ${element.tagName} element`
+      }
+      break;
   }
 }
 
@@ -59,9 +107,9 @@ export function parse_cfb(file: CFB$Container): WJSDoc {
   const buf = find(file, "/word/document.xml").content;
 
   // Parse with JSDOM
-  const dom = new JSDOM((buf as Buffer).toString(), {contentType: "text/xml"});
+  const dom = new JSDOM((buf as Buffer).toString(), { contentType: "text/xml" });
 
-  const docx: WJSDoc = {p: []}
+  const docx: WJSDoc = { p: [] }
 
   const rootelt = dom.window.document.children[0];
 
@@ -69,7 +117,7 @@ export function parse_cfb(file: CFB$Container): WJSDoc {
 
   bodyelt.childNodes.forEach(child => {
     const res = process_body_elt(child, true);
-    if(res) docx.p.push(res);
+    if (res) docx.p.push(res);
   })
 
   return docx;
